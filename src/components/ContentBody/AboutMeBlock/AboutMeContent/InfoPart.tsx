4ba5e5128df74e849ebe16e5d7fbabdc3477d0ab
Alex Pavlov <dotheimpossible@proton.com>
2023-12-10 03:19:40 +0300

    feat(AboutMeBlock): add InfoPart accordion

--- src/components/ContentBody/AboutMeBlock/AboutMeContent/InfoPart.tsx
+++ src/components/ContentBody/AboutMeBlock/AboutMeContent/InfoPart.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react'
+import { useLangStore } from 'stores'
+import { getAboutMeAccordion } from 'utils'
+import { colorScheme } from 'shared'
 
 const MainContainer = styled.div`
   display: flex;
@@ -12,16 +23,112 @@ const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* width: 100%;
-  max-width: 518px; */
-  background-color: green;
+  max-width: 610px;
+`
+const TextContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 27px;
+  width: 100%;
+`
+const StyledAccordionItem = styled(AccordionItem)<{ isActive: boolean }>`
+  background: ${colorScheme.palette.gray};
+  border: 1px solid ${colorScheme.grayFamily.gray_2};
+  border-radius: 12px;
+
+  ${({ isActive }) => isActive && `
+    background: #FFF;
+    border-radius: 0 12px 12px 0;
+  `}
+
+  &:hover {
+    ${({ isActive }) => isActive && 'background: #FFF;'}
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+const StyledAccordionButton = styled(AccordionButton)<{ isActive: boolean }>`
+  &:hover {
+    ${({ isActive }) => isActive && 'background: #FFF;'}
+    border-color: transparent;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
+const StyledBox = styled.span<{ isActive: boolean }>`
+  display: flex;
+  flex: 1;
+  text-align: left;
+  font-size: 22px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0.66px;
+
+  color: ${({ isActive }) => isActive
+    ? colorScheme.grayFamily.darkGray
+    : colorScheme.grayFamily.lightGray
+  };
+`
+const StyledAccordionPanel = styled(AccordionPanel)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  background-color: #FFF;
+  border-radius: 12px;
+`
+const StyledTextContentContainer = styled(TextContentContainer)`
 `
 
 export const InfoPart: React.FC = () => {
+  const [openItem, setOpenItem] = useState<number>(0)
+  const currLang = useLangStore((state) => state.langTheme)
+  const accordionItems = getAboutMeAccordion(currLang)
+
+  const handleItemClick = (index: number) => {
+    setOpenItem(index)
+  }
+
   return (
     <MainContainer>
       <ContentContainer>
-        <span>InfoPart</span>
+        <Accordion defaultIndex={0} onChange={handleItemClick}>
+          {accordionItems.map((item, index) => (
+            <StyledAccordionItem
+              key={item.title}
+              m={4}
+              isActive={index === openItem}
+              borderLeft={
+                index === openItem
+                  ? `3px solid ${colorScheme.palette.black}`
+                  : `1px solid ${colorScheme.grayFamily.gray_2}`
+                }
+            >
+              <span>
+                <StyledAccordionButton padding={'10px 15px'} isActive={index === openItem} _hover={'background: #FFF'}>
+                  <StyledBox isActive={index === openItem}>
+                    {item.title}
+                  </StyledBox>
+                  <AccordionIcon />
+                </StyledAccordionButton>
+              </span>
+              <StyledAccordionPanel pb={4}>
+                {item.content.map((text) => (
+                  <StyledTextContentContainer key={text}>
+                    <Box as="span" textAlign='left'>
+                      {text}
+                    </Box>
+                  </StyledTextContentContainer>
+                ))}
+              </StyledAccordionPanel>
+            </StyledAccordionItem>
+          ))}
+        </Accordion>
       </ContentContainer>
     </MainContainer>
   )
--- src/utils/getText.ts
+++ src/utils/getText.ts
@@ -2,7 +2,16 @@ import { Langs, RouteNames } from 'types'
 import jsonData from './text.json'
 
 type StartBlocks = 'title' | 'subtitle' | 'callMeButton'
-type AboutMeBlocks = 'title' | 'hello' | 'lastYears'
+type AboutMeBlocks = 'title' | 'hello' | 'lastYears' | 'accordion'
+
+type AboutMeAccordionItem = {
+  title: {[key in Langs]: string}
+  content: {[key in Langs]: string}[]
+}
+type MappedAboutMeAccordionItem = {
+  title: string
+  content: string[]
+}
 
 const textDataObj = JSON.parse(JSON.stringify(jsonData))
 
@@ -12,6 +21,11 @@ export const getStartText = (lang: Langs, block: StartBlocks) =>
   textDataObj.start[block][lang]
 export const getAboutMeText = (lang: Langs, block: AboutMeBlocks) =>
   textDataObj.aboutMe[block][lang]
+export const getAboutMeAccordion = (lang: Langs): MappedAboutMeAccordionItem[] =>
+  textDataObj.aboutMe.accordion.map((item: AboutMeAccordionItem) => ({
+    title: item.title[lang],
+    content: item.content.map((contentItem: {[key in Langs]: string}) => contentItem[lang]),
+  }))
 
 export const getNavLinks = (lang: Langs) => {
   const links = Object.keys(textDataObj.navigation).map((route) => ({
